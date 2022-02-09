b96b7d331091854e6d8718c98b248d135c724345
Ariel <ariels262626@gmail.com>
2022-02-09 10:43:10 +0200

    get posts + create post

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
+
+// app.use(bodyParser.urlencoded()) // for form data
+app.use(bodyParser.json()) // for json data
 
 app.use('/feed', feedRoutes)
 
--- routes/feed.js
+++ routes/feed.js
@@ -4,7 +4,7 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
-// /feed/posts
 router.get('/posts', feedController.getPosts)
+router.post('/post', feedController.createPost)
 
 module.exports = router
